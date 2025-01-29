e8c3ade6bb6c3c9e34039b7be8aa36575b99d5a7
AppleUdenar <appleudenar@gmail.com>
2025-01-29 17:42:04 -0500

    Update settings.py

--- backend_proyecto/settings.py
+++ backend_proyecto/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+ALLOWED_HOSTS = ['*']  # Temporal, luego usa tu dominio
+CSRF_TRUSTED_ORIGINS = ['https://*.up.railway.app']
